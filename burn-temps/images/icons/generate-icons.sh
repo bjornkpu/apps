f7d25e4a91aa0db5cf76d0fa68b909044021a190
Bjørn Kristian Punsvik <bjorn.kristian.punsvik@tietoevry.com>
2024-08-27 14:20:22 +0200

    chore: remove unused icons & update manifest
    - Remove flame-72x72.png and flame-152x152.png icons
    - Update index.html to remove references to deleted icons
    - Add new properties and categories to manifest.json
    - Update generate-icons.sh to exclude removed icon sizes

--- burn-temps/images/icons/generate-icons.sh
+++ burn-temps/images/icons/generate-icons.sh
@@ -1,4 +1,4 @@
-for size in 48 60 72 76 96 120 128 144 152 180 192 256 512; do
+for size in 48 60 76 96 120 128 144 180 192 256 512; do
     inkscape flame.svg --export-type="png" --export-filename=flame-${size}x${size}.png --export-width=${size} --export-height=${size} &
     inkscape flame-gradient.svg --export-type="png" --export-filename=flame-gradient-${size}x${size}.png --export-width=${size} --export-height=${size} &
 done
