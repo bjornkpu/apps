520020ccff30bfa4285fce3b0fe21f80b7d59503
Bjørn Kristian Punsvik <bk.punsvik@gmail.com>
2024-08-31 16:12:58 +0000

    feat: normalized graph

--- burn-temps/js/api.js
+++ burn-temps/js/api.js
@@ -35,6 +35,63 @@ export async function fetchAllRecords(burnNumberInput) {
     }
 }
 
+export async function fetchRecords(burnNumber) {
+    const url = new URL(API_BASE_URL + RECORDS_ENDPOINT);
+    url.searchParams.append('limit', 1000);
+    url.searchParams.append('where', `where=(BurnNumber,eq,${burnNumber})`);
+
+    const options = {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'xc-token': localStorage.getItem('apiKey')
+        }
+    };
+
+    try {
+        const response = await fetch(url, options);
+        if (!response.ok) {
+            throw new Error(`Error fetching records: ${response.statusText}`);
+        }
+        const data = await response.json();
+        return data.list
+    } catch (error) {
+        console.error(error.message);
+        throw error;
+    }
+}
+
+
+
+export async function fetchBurns(){
+    const url = new URL(API_BASE_URL + RECORDS_ENDPOINT);
+    url.searchParams.append('limit', 1000);
+
+    const options = {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'xc-token': localStorage.getItem('apiKey')
+        }
+    };
+
+    try {
+        const response = await fetch(url, options);
+        if (!response.ok) {
+            throw new Error(`Error fetching records: ${response.statusText}`);
+        }
+        var data = await response.json();
+        if (!data.pageInfo.isLastPage) {
+            console.error("fetchBurns function API call returned more than 1000 records. this function is void! fix it!")
+        }
+
+        return [... new Set(data.list.map(x => x.BurnNumber))]
+    } catch (error) {
+        console.error(error.message);
+        throw error;
+    }
+}
+
 export async function postRecord(record) {
     const url = new URL(API_BASE_URL + RECORDS_ENDPOINT);
     url.searchParams.append('limit', DEFAULT_LIMIT.toString());
--- burn-temps/js/graph.js
+++ burn-temps/js/graph.js
@@ -1,57 +1,68 @@
-﻿import {fetchAllRecords} from "./api.js";
+﻿import {fetchAllRecords, fetchBurns, fetchRecords} from "./api.js";
 
 (async () => {
 
     if (!localStorage.getItem('apiKey')) return
-    var records = await fetchAllRecords()
-    console.table(records.list)
 
-    var timeAxis = records.list.map(r => (new Date(r.Time)));
+    var burnNumbers = await fetchBurns();
 
-    var trace1 = {
-        type: 'line',
-        mode: 'lines+markers',
-        x: timeAxis,
-        y: records.list.map(r => r.Temp1),
-        name: 'Display',
-        line: {
-            dash: 'solid', // dot
-            color: 'rgb(82, 64, 219)',
-            width: 2
-        },
-        connectgaps: false,
+    const traces = []
+
+    for (const burn of burnNumbers){
+        const records = await fetchRecords(burn)
+        const firstRecord = new Date(records[0].Time).getTime()
+
+        var timeAxis = records.map(r => {
+            const timestamp = new Date(r.Time).getTime()
+            const normalizedTs = new Date(timestamp - firstRecord)
+            return normalizedTs.toISOString()
+        });
+        console.table(timeAxis)
+        
+        traces.push(
+            {
+                type: 'line',
+                mode: 'lines+markers',
+                x: timeAxis,
+                y: records.map(r => r.Temp1),
+                name: `Burn ${burn} - Display`,
+                line: {
+                    dash: 'solid',
+                    color: 'rgb(82, 64, 219)',
+                    width: 2
+                },
+                connectgaps: false,
+            },
+            {
+                type: 'line',
+                mode: 'lines+markers',
+                x: timeAxis,
+                y: records.map(r => r.Temp2),
+                name: `Burn ${burn} - Sidehull`,
+                line: {
+                    dash: 'dot',
+                    color: 'rgb(220, 60, 80)',
+                    width: 2
+                },
+                connectgaps: false,
+            },
+            {
+                type: 'line',
+                mode: 'lines+markers',
+                x: timeAxis,
+                y: records.map(r => r.Temp3),
+                name: `Burn ${burn} - Topphull`,
+                line: {
+                    dash: 'dot',
+                    color: 'rgb(35,163,35)',
+                    width: 2
+                },
+                connectgaps: false,
+            },
+        )
     }
 
-    var trace2 = {
-        type: 'line',
-        mode: 'lines+markers',
-        x: timeAxis,
-        y: records.list.map(r => r.Temp2),
-        name: 'Sidehull',
-        line: {
-            dash: 'dot', // dot
-            color: 'rgb(220, 60, 80)',
-            width: 2
-        },
-        connectgaps: false,
-    }
-
-    var trace3 = {
-        type: 'line',
-        mode: 'lines+markers',
-        x: timeAxis,
-        y: records.list.map(r => r.Temp3),
-        name: 'Topphull',
-        line: {
-            dash: 'dot', // dot
-            color: 'rgb(35,163,35)',
-            width: 2
-        },
-        connectgaps: false,
-    }
-
-
-    var data = [ trace1, trace2, trace3 ];
+    var data = traces;
 
     var layout = {
         title: 'Burn Graph!',
@@ -65,110 +76,12 @@
             autotick: true,
             tick0: 0,
             dtick: 100,
-            // range: [0, 1200]
         },
         font: {size: 12},
         showlegend: true,
-        // shapes: [
-        //     {
-        //         type: 'rect',
-        //         xref: 'x',
-        //         yref: 'paper',
-        //         x0: '2024-08-24 14:10:00+00:00',
-        //         y0: 0,
-        //         x1: '2024-08-24 16:30:00+00:00',
-        //         y1: 1,
-        //         fillcolor: '#FF000055',
-        //         opacity: 0.2,
-        //         editable: true,
-        //         line: {
-        //             width: 0,
-        //         },
-        //         label: {
-        //             text: 'Heat',
-        //             font: { size: 10, color: 'green' },
-        //             textposition: 'top center',
-        //         },
-        //     },
-        //     {
-        //         type: 'rect',
-        //         xref: 'x',
-        //         yref: 'paper',
-        //         x0: '2024-08-24 16:30:00+00:00',
-        //         y0: 0,
-        //         x1: '2024-08-24 18:25:00+00:00',
-        //         y1: 1,
-        //         fillcolor: '#FF000099',
-        //         opacity: 0.2,
-        //         editable: true,
-        //         line: {
-        //             width: 0,
-        //         },
-        //         label: {
-        //             text: 'Max temp',
-        //             font: { size: 10, color: 'green' },
-        //             textposition: 'top center',
-        //         },
-        //     },
-        //     {
-        //         type: 'rect',
-        //         xref: 'x',
-        //         yref: 'paper',
-        //         x0: '2024-08-24 18:25:00+00:00',
-        //         y0: 0,
-        //         x1: '2024-08-24 18:55:00+00:00',
-        //         y1: 1,
-        //         fillcolor: '#FF0000FF',
-        //         opacity: 0.2,
-        //         editable: true,
-        //         line: {
-        //             width: 0,
-        //         },
-        //         label: {
-        //             text: 'Hold',
-        //             font: { size: 10, color: 'green' },
-        //             textposition: 'top center',
-        //         },
-        //     },
-        //     {
-        //         type: 'rect',
-        //         xref: 'x',
-        //         yref: 'paper',
-        //         x0: '2024-08-24 18:55:00+00:00',
-        //         y0: 0,
-        //         x1: '2024-08-25 12:00:00+00:00',
-        //         y1: 1,
-        //         fillcolor: '#0000FF55',
-        //         opacity: 0.2,
-        //         editable: true,
-        //         line: {
-        //             width: 0,
-        //         },
-        //         label: {
-        //             text: 'Cool',
-        //             font: { size: 10, color: 'green' },
-        //             textposition: 'top center',
-        //         },
-        // },
-        // {
-        //     type: 'line',
-        //     x0: '2015-02-01',
-        //     y0: 8,
-        //     x1: '2015-02-28',
-        //     y1: 8,
-        //     fillcolor: '#d3d3d3',
-        //     opacity: 0.2,
-        //     editable: true,
-        //     label: {
-        //         text: 'January average',
-        //         yanchor: 'top',
-        //     },
-        // },
-        // ],
     };
 
     var config = {responsive: true}
-
 
     Plotly.newPlot('graph', data, layout, config );
 
