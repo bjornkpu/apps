4b2eba43e89d52b585df94b2d34bed78a1d2dd44
Bjørn Kristian Punsvik <bk.punsvik@gmail.com>
2024-08-31 15:19:09 +0000

    refactor: clean up html and fetch api

--- burn-temps/js/api.js
+++ burn-temps/js/api.js
@@ -6,13 +6,13 @@ const DEFAULT_SHUFFLE = 0;
 const DEFAULT_OFFSET = 0;
 
 
-export async function fetchAllRecords({ limit = DEFAULT_LIMIT, shuffle = DEFAULT_SHUFFLE, offset = DEFAULT_OFFSET } = {}) {
-    var burnNumber = localStorage.getItem('burnNumber')
+export async function fetchAllRecords(burnNumberInput) {
+    var burnNumber = burnNumberInput ?? localStorage.getItem('burnNumber')
 
     const url = new URL(API_BASE_URL + RECORDS_ENDPOINT);
-    url.searchParams.append('limit', limit);
-    url.searchParams.append('shuffle', shuffle);
-    url.searchParams.append('offset', offset);
+    url.searchParams.append('limit', 1000);
+    // url.searchParams.append('shuffle', shuffle);
+    // url.searchParams.append('offset', offset);
     url.searchParams.append('where', `where=(BurnNumber,eq,${burnNumber || 1})`);
 
     const options = {
--- burn-temps/js/graph.js
+++ burn-temps/js/graph.js
@@ -3,10 +3,8 @@
 (async () => {
 
     if (!localStorage.getItem('apiKey')) return
-    var records = await fetchAllRecords({limit:1000})
+    var records = await fetchAllRecords()
     console.table(records.list)
-    var message = 'Hello World!';
-    console.log(message);
 
     var timeAxis = records.list.map(r => (new Date(r.Time)));
 
@@ -67,106 +65,106 @@
             autotick: true,
             tick0: 0,
             dtick: 100,
-            range: [0, 1200]
+            // range: [0, 1200]
         },
         font: {size: 12},
         showlegend: true,
-        shapes: [
-            {
-                type: 'rect',
-                xref: 'x',
-                yref: 'paper',
-                x0: '2024-08-24 14:10:00+00:00',
-                y0: 0,
-                x1: '2024-08-24 16:30:00+00:00',
-                y1: 1,
-                fillcolor: '#FF000055',
-                opacity: 0.2,
-                editable: true,
-                line: {
-                    width: 0,
-                },
-                label: {
-                    text: 'Heat',
-                    font: { size: 10, color: 'green' },
-                    textposition: 'top center',
-                },
-            },
-            {
-                type: 'rect',
-                xref: 'x',
-                yref: 'paper',
-                x0: '2024-08-24 16:30:00+00:00',
-                y0: 0,
-                x1: '2024-08-24 18:25:00+00:00',
-                y1: 1,
-                fillcolor: '#FF000099',
-                opacity: 0.2,
-                editable: true,
-                line: {
-                    width: 0,
-                },
-                label: {
-                    text: 'Max temp',
-                    font: { size: 10, color: 'green' },
-                    textposition: 'top center',
-                },
-            },
-            {
-                type: 'rect',
-                xref: 'x',
-                yref: 'paper',
-                x0: '2024-08-24 18:25:00+00:00',
-                y0: 0,
-                x1: '2024-08-24 18:55:00+00:00',
-                y1: 1,
-                fillcolor: '#FF0000FF',
-                opacity: 0.2,
-                editable: true,
-                line: {
-                    width: 0,
-                },
-                label: {
-                    text: 'Hold',
-                    font: { size: 10, color: 'green' },
-                    textposition: 'top center',
-                },
-            },
-            {
-                type: 'rect',
-                xref: 'x',
-                yref: 'paper',
-                x0: '2024-08-24 18:55:00+00:00',
-                y0: 0,
-                x1: '2024-08-25 12:00:00+00:00',
-                y1: 1,
-                fillcolor: '#0000FF55',
-                opacity: 0.2,
-                editable: true,
-                line: {
-                    width: 0,
-                },
-                label: {
-                    text: 'Cool',
-                    font: { size: 10, color: 'green' },
-                    textposition: 'top center',
-                },
-            },
-            // {
-            //     type: 'line',
-            //     x0: '2015-02-01',
-            //     y0: 8,
-            //     x1: '2015-02-28',
-            //     y1: 8,
-            //     fillcolor: '#d3d3d3',
-            //     opacity: 0.2,
-            //     editable: true,
-            //     label: {
-            //         text: 'January average',
-            //         yanchor: 'top',
-            //     },
-            // },
-        ],
+        // shapes: [
+        //     {
+        //         type: 'rect',
+        //         xref: 'x',
+        //         yref: 'paper',
+        //         x0: '2024-08-24 14:10:00+00:00',
+        //         y0: 0,
+        //         x1: '2024-08-24 16:30:00+00:00',
+        //         y1: 1,
+        //         fillcolor: '#FF000055',
+        //         opacity: 0.2,
+        //         editable: true,
+        //         line: {
+        //             width: 0,
+        //         },
+        //         label: {
+        //             text: 'Heat',
+        //             font: { size: 10, color: 'green' },
+        //             textposition: 'top center',
+        //         },
+        //     },
+        //     {
+        //         type: 'rect',
+        //         xref: 'x',
+        //         yref: 'paper',
+        //         x0: '2024-08-24 16:30:00+00:00',
+        //         y0: 0,
+        //         x1: '2024-08-24 18:25:00+00:00',
+        //         y1: 1,
+        //         fillcolor: '#FF000099',
+        //         opacity: 0.2,
+        //         editable: true,
+        //         line: {
+        //             width: 0,
+        //         },
+        //         label: {
+        //             text: 'Max temp',
+        //             font: { size: 10, color: 'green' },
+        //             textposition: 'top center',
+        //         },
+        //     },
+        //     {
+        //         type: 'rect',
+        //         xref: 'x',
+        //         yref: 'paper',
+        //         x0: '2024-08-24 18:25:00+00:00',
+        //         y0: 0,
+        //         x1: '2024-08-24 18:55:00+00:00',
+        //         y1: 1,
+        //         fillcolor: '#FF0000FF',
+        //         opacity: 0.2,
+        //         editable: true,
+        //         line: {
+        //             width: 0,
+        //         },
+        //         label: {
+        //             text: 'Hold',
+        //             font: { size: 10, color: 'green' },
+        //             textposition: 'top center',
+        //         },
+        //     },
+        //     {
+        //         type: 'rect',
+        //         xref: 'x',
+        //         yref: 'paper',
+        //         x0: '2024-08-24 18:55:00+00:00',
+        //         y0: 0,
+        //         x1: '2024-08-25 12:00:00+00:00',
+        //         y1: 1,
+        //         fillcolor: '#0000FF55',
+        //         opacity: 0.2,
+        //         editable: true,
+        //         line: {
+        //             width: 0,
+        //         },
+        //         label: {
+        //             text: 'Cool',
+        //             font: { size: 10, color: 'green' },
+        //             textposition: 'top center',
+        //         },
+        // },
+        // {
+        //     type: 'line',
+        //     x0: '2015-02-01',
+        //     y0: 8,
+        //     x1: '2015-02-28',
+        //     y1: 8,
+        //     fillcolor: '#d3d3d3',
+        //     opacity: 0.2,
+        //     editable: true,
+        //     label: {
+        //         text: 'January average',
+        //         yanchor: 'top',
+        //     },
+        // },
+        // ],
     };
 
     var config = {responsive: true}
